b6b8d8629040a195c27759b4798da604f4991dc2
Stuart Swerdloff <sjswerdloff@gmail.com>
2020-11-24 21:39:03 +1300

    Initial Commit

---
+++ Gmail Reverse Conversation/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Gmail Reverse Conversation
+//
+//  Created by Stuart Swerdloff on 24/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
